f23a605a2657005545147ddac6f4df920cd7de7f
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2020-01-15 19:57:14 -0700

    updated styles

---
+++ assets/js/animations.js
@@ -0,0 +1,22 @@
+
+// $(document).ready(function(){
+//   $("#showroomQuote").hide()
+// });
+
+// $(document).ready(function(){
+//     $(window).scroll(function() {
+//         $("#showroomQuote").show().fadeIn("slow", function(){
+//           // Code to be executed
+//           alert("The show effect is completed.");
+//       });
+//     })
+//   });
+  
+  
+  
+  $("div").scroll(function(){
+    $("#showroomQuote").show().fadeIn("slow")
+      // Code to be executed
+      // alert("The show effect is completed.");
+
+});
