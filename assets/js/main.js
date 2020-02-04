65a04aae9253f4bbb20540a849c510bd8e5a97aa
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2020-02-03 23:16:19 -0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -209,5 +209,79 @@
 				resetForms: true,
 				side: 'right'
 			});
+			
+// Gallery
+			$(function() {
+                  
+				$('.lazy').lazy({
+					// your configuration goes here
+					scrollDirection: 'vertical',
+					effect: 'fadeIn',
+					visibleOnly: true,
+				 
+					onError: function(element) {
+							console.log('error loading ' + element.data('src'));
+					}
+				});
+						
+				});
+			
+				$(window).on('load', (function() {
+			
+			$(".allFilter").show();
+			$(".straightFilter").hide();
+			$(".circularFilter").hide();
+			$(".otherFilter").hide();
+			$(".deckFilter").hide();
+			$("#galleryTitle").html("Our Work")
+			
+			$("#filterSelect").change(function(){
+			if( $(this).val() == "All" ) { 
+			$(".allFilter").show();
+			$(".straightFilter").hide();
+			$(".circularFilter").hide();
+			$(".otherFilter").hide();
+			$(".deckFilter").hide()
+			$("#galleryTitle").html("Our Work")
+			}
+			
+			else if ( $(this).val() == "Straight Stairs" ) { 
+			$(".allFilter").hide();
+			$(".straightFilter").show();
+			$(".circularFilter").hide();
+			$(".otherFilter").hide();
+			$(".deckFilter").hide()
+			$("#galleryTitle").html("Straight Stairs")
+			}
+			else if( $(this).val() == "Circular Stairs" ) { 
+			$(".allFilter").hide();
+			$(".straightFilter").hide();
+			$(".circularFilter").show();
+			$(".otherFilter").hide();
+			$(".deckFilter").hide()
+			$("#galleryTitle").html("Circular Stairs")
+			}
+			else if( $(this).val() == "Outdoor Stairs" ) { 
+			$(".allFilter").hide();
+			$(".straightFilter").hide();
+			$(".circularFilter").hide();
+			$(".otherFilter").hide();
+			$(".deckFilter").show()
+			$("#galleryTitle").html("Outdoor Stairs")
+			}
+			else if( $(this).val() == "Spindles, Posts & Rails" ) { 
+			$(".allFilter").hide();
+			$(".straightFilter").hide();
+			$(".circularFilter").hide();
+			$(".otherFilter").show();
+			$(".deckFilter").hide()
+			$("#galleryTitle").html("Spindles, Posts & Rails")
+			}
+			
+			}); 
+			
+			})
+				);
+				
 
 })(jQuery);
