a1ba483064851059ea5387a6e93fe996b3156e56
Mallory Zlomanchuk <mzlomanchuk@hotmail.com>
2020-02-08 17:00:15 -0700

    quote page

--- assets/js/quote.js
+++ assets/js/quote.js
@@ -4,6 +4,9 @@ $("#advanced").hide();
 $("#balustrade").hide();
 $("#file").hide();
 $("#stairMeasurements").hide();
+$("#stairStyleForm").hide();
+$("#quoteForm").hide();
+$("#step").html('Step 1: Choose Your Stairs');
 
 }));
 
@@ -26,5 +29,55 @@ $("#fileButton").click(function(){
 $("#stairButton").click(function(){
   $("#stairMeasurements").toggle();
 
+});
+
+$("#straightStairButton").click(function(){
+  $("#quoteContainer").hide();
+  $("#stairStyleForm").show();
+  $("#step").html('Step 2: Choose Your Style of Stairs')
+
+});
+
+$("#outdoorStairButton").click(function(){
+  $("#quoteContainer").hide();
+  $("#stairStyleForm").show();
+  $("#step").html('Step 2: Choose Your Style of Stairs')
+
+});
+
+$("#constructionStairButton").click(function(){
+  $("#quoteContainer").hide();
+  $("#stairStyleForm").show();
+  $("#step").html('Step 2: Choose Your Style of Stairs')
+
+});
+
+$("#customStairButton").click(function(){
+  $("#quoteContainer").hide();
+  $("#quoteForm").show();
+
+});
+
+$("#back1").click(function(){
+  $("#quoteContainer").show();
+  $("#stairStyleForm").hide();
+
+});
+
+$("#questionA").click(function(){
+  $("#answerA").html('This is the total height from top of floor to top of floor.')
+});
+
+$("#questionB").click(function(){
+  $("#answerB").html('This is the total length of the stair from where it starts to where you want it to end on the floor below.')
+});
+
+
+$("#questionC").click(function(){
+  $("#answerC").html('This is the total width of the stair.')
+});
+
+$("#questionD").click(function(){
+  $("#answerD").html('The horizontal distance from where you start to walk down the stairs to the point where you could hit your head.')
 });
 
